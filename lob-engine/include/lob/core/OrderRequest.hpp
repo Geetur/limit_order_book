0a7bbbbdc827978400192f25bce68aef1da4cc30
Jeter Pontes <jeter.pontes@outlook.com>
2026-05-04 21:20:54 -0700

    Add OrderRequest and ExecutionReport structures; set up tests for ingress/engine/egress threads

---
+++ lob-engine/include/lob/core/OrderRequest.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cstdint>
+
+namespace lob::core {
+struct  OrderRequest{
+
+    uint64_t order_id; // 8 byte
+    uint64_t price; // 8 byte
+    uint32_t quantity; // 4 byte
+    bool is_buy; // 1 byte
+
+};
+};
